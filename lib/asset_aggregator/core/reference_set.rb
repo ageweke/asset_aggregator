5c48656ea7fd3ac0367477f9a11425d859fdd8a6
Andrew Geweke <andrew_github@geweke.org>
2009-12-17 14:37:01 -0800

    Add support for AggregateReference objects to the ReferenceSet, and sort references on output.

--- lib/asset_aggregator/core/reference_set.rb
+++ lib/asset_aggregator/core/reference_set.rb
@@ -1,11 +1,11 @@
 module AssetAggregator
   module Core
-    # A #ReferenceSet is a collection of #FragmentReference objects. It's used
-    # to implement reference tracking on a per-page basis: a single instance of
-    # this class (actually, #FreezableReferenceSet) is created and added to the
-    # #ApplicationController on each request. As controller methods are called
-    # and views are rendered, references can be added to this object -- either
-    # explicitly, or implicitly, in cases where +foo.html.erb+ is set to
+    # A #ReferenceSet is a collection of #FragmentReference or #AggregateReference
+    # objects. It's used  to implement reference tracking on a per-page basis: a
+    # single instance of this class (actually, #FreezableReferenceSet) is created
+    # and used by the #PageReferenceSet on each request. As controller methods are
+    # called and views are rendered, references can be added to this object -- 
+    # either explicitly, or implicitly, in cases where +foo.html.erb+ is set to
     # automatically pick up, for example, +foo.js+ and +foo.css+. 
     #
     # When the page is complete, the layout calls +each_aggregate_reference+,
@@ -14,6 +14,12 @@ module AssetAggregator
     # are needed for the page, automatically, along with (if desired) a list of
     # references to fragments that are aggregated in that subpath (i.e., why
     # that subpath is required).
+    #
+    # Usually, you'll add #FragmentReference objects to this set. However, there
+    # are also #AggregateReference objects, in cases where you explicitly want to
+    # require an aggregate rather than a single fragment. This couples code with
+    # knowledge of what fragments are ending up in what aggregates, and so is 
+    # generally a bad idea, but it's there if you need it.
     class ReferenceSet
       # Creates a new, empty instance.
       def initialize
@@ -25,8 +31,16 @@ module AssetAggregator
         @references << reference unless @references.include?(reference)
       end
       
+      # Returns an array of symbols, which is the set of all distinct aggregate types
+      # that this #ReferenceSet has references to in it. This is sorted alphabetically,
+      # in order to comply with our "always be deterministic" principle.
+      def aggregate_types
+        @references.map { |r| r.aggregate_type }.uniq.sort_by { |t| t.to_s }
+      end
+      
       # Yields, in turn, both a subpath (a #String) and an #Array of all #FragmentReference
       # objects added to this set that refer to content that's aggregated under that
+      # subpath, plus all #AggregateReference objects that explicitly refer to that
       # subpath. The +aggregate_type_symbol+ says what kind of content you want;
       # the +asset_aggregator+ is a reference to the top-level #AssetAggregator
       # object itself. (We need this so that we can go ask it where each of the
@@ -38,25 +52,12 @@ module AssetAggregator
         subpath_to_reference_map = { }
         
         @references.select { |r| r.aggregate_type == aggregate_type_symbol }.each do |reference|
-          subpath = asset_aggregator.aggregated_subpath_for(aggregate_type_symbol, reference.fragment_source_position)
-          unless subpath
-            raise %{You declared a reference,
-#{reference},
-that points to a fragment,
-#{reference.fragment_source_position},
-that isn't actually aggregated by the AssetAggregator!
-
-You'll need to change your AssetAggregator configuration so that data in
-#{reference.fragment_source_position}
-is aggregated, so that when you declare this reference, we know what
-aggregate we need to include.}
-          end
-          
+          subpath = reference.aggregate_subpath(asset_aggregator)
           subpath_to_reference_map[subpath] ||= [ ]
           subpath_to_reference_map[subpath] << reference
         end
         
-        subpath_to_reference_map.keys.sort.each { |k| block.call(k, subpath_to_reference_map[k]) }
+        subpath_to_reference_map.keys.sort.each { |k| block.call(k, subpath_to_reference_map[k].sort) }
       end
     end
   end
--- spec/asset_aggregator/core/reference_set_spec.rb
+++ spec/asset_aggregator/core/reference_set_spec.rb
@@ -26,13 +26,15 @@ describe AssetAggregator::Core::ReferenceSet do
     ref3 = make_ref(:foo, 'baz', 'bonk', 'whatever')
     ref4 = make_ref(:foo, 'aaa', 'baz', 'more')
     ref5 = make_ref(:bar, 'bar', 'baz', 'something else')
+    ref6 = make_ref(:foo, 'abc', 'hooo', 'something else')
     
-    [ @ref1, ref2, ref3, ref4, ref5 ].each { |r| @reference_set.add(r) }
+    [ @ref1, ref2, ref3, ref4, ref5, ref6 ].each { |r| @reference_set.add(r) }
     
     asset_aggregator = mock(:asset_aggregator)
     asset_aggregator.should_receive(:aggregated_subpath_for).with(:foo, @ref1.fragment_source_position).and_return('agg_1')
     asset_aggregator.should_receive(:aggregated_subpath_for).with(:foo, ref3.fragment_source_position).and_return('agg_2')
     asset_aggregator.should_receive(:aggregated_subpath_for).with(:foo, ref4.fragment_source_position).and_return('agg_1')
+    asset_aggregator.should_receive(:aggregated_subpath_for).with(:foo, ref6.fragment_source_position).and_return('agg_1')
     
     output = [ ]
     @reference_set.each_aggregate_reference(:foo, asset_aggregator) do |subpath, references|
@@ -43,14 +45,23 @@ describe AssetAggregator::Core::ReferenceSet do
 
     output[0][0].should == 'agg_1'
     references = output[0][1]
-    references.length.should == 2
+    references.length.should == 3
     references[0].reference_source_position.file.should == File.canonical_path('baz')
-    references[1].reference_source_position.file.should == File.canonical_path('baz')
+    references[1].reference_source_position.file.should == File.canonical_path('hooo')
+    references[2].reference_source_position.file.should == File.canonical_path('baz')
     
     output[1][0].should == 'agg_2'
     references = output[1][1]
     references.length.should == 1
     references[0].reference_source_position.file.should == File.canonical_path('bonk')
+  end
+  
+  it "should return the set of aggregate types correctly" do
+    @reference_set.add(@ref1)
+    @reference_set.add(make_ref(:foo, 'bar', 'bonk', 'whatever'))
+    @reference_set.add(make_ref(:bar, 'bar', 'haha', 'yo'))
+    
+    @reference_set.aggregate_types.should == [ :bar, :foo ]
   end
   
   it "should just not yield anything for subpaths with no data" do
