fd49ff1d00579a9a1a130425f721cd2613fbd415
Andrew Geweke <andrew_github@geweke.org>
2010-01-05 14:29:56 -0800

    Allow passing static filenames to javascript_include_aggregate and stylesheet_link_aggregate, since the asset_packager versions let you do that and we (sadly) take advantage of that.

--- lib/asset_aggregator/rails/asset_packager_compatibility_helper.rb
+++ lib/asset_aggregator/rails/asset_packager_compatibility_helper.rb
@@ -14,21 +14,53 @@ module AssetAggregator
       alias_method :stylesheet_link_merged, :stylesheet_link_aggregate
       
       private
-      def merged_tags(sources, tag_method, asset_type, expand_sources)
+      def public_directory_for(aggregate_type)
+        case aggregate_type
+        when :javascript then 'javascripts'
+        when :css then 'stylesheets'
+        else raise("Don't know what directory under public/ contains data for type #{aggregate_type.inspect}")
+        end
+      end
+      
+      def with_extension(aggregate_type, filename)
+        extension = case aggregate_type
+        when :javascript then 'js'
+        when :css then 'css'
+        else raise("Don't know what extension data of type #{aggregate_type.inspect} has")
+        end
+        
+        filename += ".#{extension}" unless filename =~ /\.#{extension}$/i
+        filename
+      end
+      
+      def merged_tags(sources, tag_method, aggregate_type, expand_sources)
         options = sources.last.is_a?(Hash) ? sources.pop.stringify_keys : { }
         sources = sources.map { |source| expand_source(source) } if expand_sources
         sources = sources.flatten
         
+        available_subpaths = AssetAggregator.all_subpaths(aggregate_type)
+        
         sources.map do |source|
-          mtime = AssetAggregator.mtime_for(asset_type, source.to_s)
-          url = AssetAggregator.aggregate_url(method(:url_for), asset_type, source.to_s)
-          if url =~ /\?/
-            url += "&_aamt=#{mtime.to_i}"
-          else
-            url += "?#{mtime.to_i}"
-          end
+          mtime = nil
+          mtime = AssetAggregator.mtime_for(aggregate_type, source.to_s) if available_subpaths.include?(source.to_s)
           
-          tag_method.call(url, options)
+          if mtime && mtime > 0
+            url = AssetAggregator.aggregate_url(method(:url_for), aggregate_type, source.to_s)
+            if url =~ /\?/
+              url += "&_aamt=#{mtime.to_i}"
+            else
+              url += "?#{mtime.to_i}"
+            end
+          
+            tag_method.call(url, options)
+          else
+            target_file = File.join(::Rails.root, 'public', public_directory_for(aggregate_type), with_extension(aggregate_type, source.to_s))
+            if File.exist?(target_file)
+              tag_method.call(source.to_s, options)
+            else
+              raise "You asked to link to #{source.inspect}, but we found neither an aggregate nor a single file under public/ with that name. (We looked for: '#{target_file}')"
+            end
+          end
         end.join("\n")
       end
       
