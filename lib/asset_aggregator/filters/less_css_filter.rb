8f6754f62b3cf6df2d7c849d2345791f8bda80b0
Andrew Geweke <andrew_github@geweke.org>
2010-01-11 13:34:49 -0800

    Make the LessCssFilter take a :processing callback, so we can output timings.

--- lib/asset_aggregator/filters/less_css_filter.rb
+++ lib/asset_aggregator/filters/less_css_filter.rb
@@ -3,13 +3,23 @@ module AssetAggregator
     # A simple Filter that uses Less (http://lesscss.org/) to process 'less'
     # code into normal CSS.
     class LessCssFilter < AssetAggregator::Core::Filter
-      # Creates a new instance. If you pass text in 'prefix', it will get prepended
-      # to the source before it gets processed through 'less'. This is intended to
-      # allow you to add automatic @import statements, variable settings, etc.;
-      # adding actual CSS rules in there is likely a Very Bad Idea (tm).
-      def initialize(prefix = "")
-        @prefix = prefix
+      # Creates a new instance. 
+      #
+      # +options+ can contain any of:
+      #   * +:prefix+ -- if supplied, prepended to the input (with a newline
+      #     between) before it gets processed through 'less'. This is intended to
+      #     allow you to add automatic @import statements, variable settings, etc.;
+      #     adding actual CSS rules in there is likely a Very Bad Idea (tm).
+      #   * +:processing+ -- if supplied, called with the #Fragment being
+      #     filtered, the entire (#String) contents of the input being filtered,
+      #     and the amount of time taken to do the processing, after each call.
+      def initialize(options = { })
+        @options = options
+        
+        @prefix = options[:prefix]
         @prefix += "\n" unless @prefix.blank? || @prefix[-1..-1] == "\n"
+        
+        @processing = options[:processing] || (Proc.new { |fragment, input, time| })
       end
       
       def filter(fragment, input)
@@ -17,7 +27,12 @@ module AssetAggregator
         net_input = (@prefix || "") + input
         
         begin
-          Less.parse(net_input)
+          start_time = Time.now
+          out = Less.parse(net_input)
+          end_time = Time.now
+          
+          @processing.call(fragment, input, end_time - start_time)
+          out
         rescue => e
           raise "Unable to process CSS using Less; got: #{e} with input:\n#{net_input}"
         end
--- spec/asset_aggregator/filters/less_css_filter_spec.rb
+++ spec/asset_aggregator/filters/less_css_filter_spec.rb
@@ -3,9 +3,9 @@ require 'spec/spec_helper'
 # This spec is NOT meant to test all the features of less; that's a job
 # for the specs in less itself. 
 describe AssetAggregator::Filters::LessCssFilter do
-  def filter(input, prefix = nil)
-    fragment = mock(:fragment)
-    AssetAggregator::Filters::LessCssFilter.new(prefix).filter(fragment, input)
+  def filter(input, options = { })
+    @fragment = mock(:fragment)
+    AssetAggregator::Filters::LessCssFilter.new(options).filter(@fragment, input)
   end
   
   it "should process input through less" do
@@ -13,6 +13,25 @@ describe AssetAggregator::Filters::LessCssFilter do
   end
   
   it "should add a prefix where appropriate" do
-    filter(".foo { .bar { color: @my_color; } }", "@my_color: red;").strip.should == ".foo .bar { color: red; }"
+    filter(".foo { .bar { color: @my_color; } }", :prefix => "@my_color: red;").strip.should == ".foo .bar { color: red; }"
+  end
+  
+  it "should call back on the :processing proc" do
+    processing_calls = [ ]
+    processing = Proc.new { |fragment, input, time| processing_calls << [ fragment, input, time ] }
+    
+    filter(".foo { .bar { color: red } }", :processing => processing).strip.should == ".foo .bar { color: red; }"
+    processing_calls.length.should == 1
+    processing_calls[0][0].should == @fragment
+    processing_calls[0][1].should == ".foo { .bar { color: red } }"
+    processing_calls[0][2].should >= 0
+    processing_calls[0][2].should <= 0.2
+    
+    filter(".foo { .bar { color: blue } }", :processing => processing).strip.should == ".foo .bar { color: blue; }"
+    processing_calls.length.should == 2
+    processing_calls[1][0].should == @fragment
+    processing_calls[1][1].should == ".foo { .bar { color: blue } }"
+    processing_calls[1][2].should >= 0
+    processing_calls[1][2].should <= 0.2
   end
 end
