7bc817698c0e56b7f88633683c7a5f1f8334c531
Andrew Geweke <andrew_github@geweke.org>
2010-03-19 13:15:05 -0700

    Finish spec and comments for #PageReferenceSet.

--- lib/asset_aggregator/core/page_reference_set.rb
+++ lib/asset_aggregator/core/page_reference_set.rb
@@ -1,11 +1,38 @@
 module AssetAggregator
   module Core
+    # A #PageReferenceSet is a wrapper around a #ReferenceSet that provides methods with
+    # friendly APIs for adding dependencies to it. It also can use a #PageReferencesOutputHandler
+    # to produce HTML markup that's appropriate for including into a page -- that is, the
+    # actual text of the set of <script src="..."> tags or <link rel="stylesheet" ...> tags
+    # you need in order to get the assets onto your page.
     class PageReferenceSet
+      # Creates a new instance. +asset_aggregator+ is the #AssetAggregator instance to use;
+      # we need it to map fragment references back to subpaths and to figure out exactly
+      # which subpaths to include, as well as for configuration data, etc.
+      #
+      # A new instance of this class should generally be used for each page you generate.
+      # The idea is that you'll create it (perhaps lazily) as a +before_filter+ on your
+      # #ApplicationController, call #require_fragment (ideally) or #require_aggregate
+      # (when absolutely necessary; generally you'd like to have fine-grained dependencies,
+      # so you know *why* a particular aggregate gets included, rather than requiring the
+      # whole damn thing manually) throughout your code as things get rendered and
+      # controller actions get hit, and then call #include_text on it from your layout,
+      # plunking the resulting tags in the right places on your page.
       def initialize(asset_aggregator)
         @asset_aggregator = asset_aggregator
         @reference_set = AssetAggregator::Core::FreezableReferenceSet.new(integration)
       end
       
+      # Tells this #PageReferenceSet that, for the given +aggregate_type+ (like
+      # :javascript or :css), a fragment in +fragment_file+ (either a straight-up filename,
+      # either absolute or relative to #Integration's base directory, or that plus a colon
+      # and a line number, if referring to a fragment with a line number) is needed by
+      # code originating at +source_position+. If not supplied, +source_position+ defaults
+      # to the caller of this method. +descrip+ can be a human-readable string explaining
+      # *why* the fragment in question is required; if not supplied, it defaults to
+      # "explicit reference", implying that code directly stated that the fragment was
+      # required (as opposed to being required because we're rendering an associated
+      # view, or something like that).
       def require_fragment(aggregate_type, fragment_file, source_position = nil, descrip = nil)
         descrip ||= "explicit reference"
         source_position ||= AssetAggregator::Core::SourcePosition.levels_up_stack(2)
@@ -22,6 +49,29 @@ module AssetAggregator
         @reference_set.add(AssetAggregator::Core::FragmentReference.new(aggregate_type, fragment_file, source_position, descrip))
       end
       
+      # Tells this #PageReferenceSet that, for the given +aggregate_type+ (like :javascript
+      # or :css), the entire aggregate at the given +aggregate_subpath+ is required.
+      #
+      # Generally, you shouldn't do this: part of the point of the dependency-tracking
+      # mechanisms of the #AssetAggregator is that you tell it what _fragments_ you need,
+      # and let it figure out the best set of aggregates to include that will cover those
+      # fragments. However, if you have lots of legacy code that can't really register its
+      # dependencies correctly, or if you have special situations, you can explicitly require
+      # an aggregate this way.
+      #
+      # By doing this, note that you lose three concrete things:
+      #   * You can't know what _part_ of the aggregate is required; if you decide later
+      #     you want to change the aggregation rules to optimize inclusion behavior,
+      #     you can't figure out what you can put in different groupings to make things
+      #     faster or better;
+      #   * You constrain the aggregate-selection algorithm in the #ReferenceSet; because
+      #     you're fixing certain aggregates, other combinations are not possible; and
+      #   * You can't pass +:include_fragment_dependencies_instead_of_aggregates+ to
+      #     the options for #include_text (in order to check whether your dependencies
+      #     are really all there), and thus get _just_ the fragments themselves
+      #     included -- because you're explicitly requiring an aggregate, we can't really
+      #     include this _and_ fragments themselves, because we'd very likely double-
+      #     include one or more fragments. 
       def require_aggregate(aggregate_type, aggregate_subpath, source_position = nil, descrip = nil)
         descrip ||= "explicit reference"
         source_position ||= AssetAggregator::Core::SourcePosition.levels_up_stack(2)
@@ -29,6 +79,19 @@ module AssetAggregator
         @reference_set.add(AssetAggregator::Core::AggregateReference.new(aggregate_type, aggregate_subpath, source_position, descrip))
       end
       
+      # Returns HTML markup that will include all necessary aggregates for this page.
+      # This is guaranteed to include aggregates that cover at least all fragments
+      # you have added dependencies for (using #require_fragment), plus any aggregates
+      # you've added explicit dependencies for (using #require_aggregate). 
+      #
+      # The actual text creation is done by a separate output-handler class, which
+      # defaults to #PageReferencesOutputHandler but which can be overridden by passing
+      # the class in as +:output_handler_class+ in the +options+.
+      #
+      # You can restrict the output to only a particular kind of content, by passing
+      #   options[:types] => [ :javascript, :css ]
+      # ...or something similar. All other options are passed through to the constructor
+      # of the output-handler class and can be used to modify its output.
       def include_text(options = { })
         options = options.dup
         
@@ -58,10 +121,12 @@ module AssetAggregator
       end
       
       private
+      # The #AssetAggregator we're working for.
       def asset_aggregator
         @asset_aggregator
       end
       
+      # The #Integration object we should use.
       def integration
         @asset_aggregator.integration
       end
--- spec/asset_aggregator/core/page_reference_set_spec.rb
+++ spec/asset_aggregator/core/page_reference_set_spec.rb
@@ -6,64 +6,202 @@ describe AssetAggregator::Core::PageReferenceSet do
     @asset_aggregator = mock(:asset_aggregator, :integration => @integration)
     
     @javascript_type = mock(:javascript_type)
-     @asset_aggregator.should_receive(:aggregate_type).any_number_of_times.with(:javascript).and_return(@javascript_type)
-    
+    @css_type = mock(:css_type)
+    @asset_aggregator.should_receive(:aggregate_type).any_number_of_times do |in_type|
+      case in_type
+      when :javascript then @javascript_type
+      when :css then @css_type
+      else raise("Unknown type #{in_type}")
+      end
+    end
+
     @output_handler = mock(:output_handler)
     @output_handler_class = mock(:output_handler_class)
     
     @set = AssetAggregator::Core::PageReferenceSet.new(@asset_aggregator)
   end
   
-  def add_fragment(type, path, new_path, descrip, target_subpaths)
-    @integration.should_receive(:path_from_base).once.with(path).and_return(new_path)
-    source_position = mock(:source_position)
+  def add_fragment(type, path, new_path, descrip, target_subpaths, source_position = nil, skip_aggregated_subpaths = false)
+    passed_path = path
+    passed_line = nil
+    if passed_path =~ /^(.*):(\d+)$/i
+      passed_path, passed_line = $1, $2.to_i
+    end
+    @integration.should_receive(:path_from_base).once.with(passed_path).and_return(new_path)
+    source_position ||= mock(:source_position)
     @set.require_fragment(type, path, source_position, descrip)
 
-    @asset_aggregator.should_receive(:aggregated_subpaths_for).once do |actual_type, actual_source_position|
-      actual_type.should == type
-      actual_source_position.file.should == File.expand_path(new_path)
-      target_subpaths
+    unless skip_aggregated_subpaths
+      @asset_aggregator.should_receive(:aggregated_subpaths_for).once do |actual_type, actual_source_position|
+        actual_type.should == type
+        actual_source_position.file.should == File.expand_path(new_path)
+        actual_source_position.line.should == passed_line
+        target_subpaths
+      end
     end
 
+    source_position
+  end
+  
+  def add_aggregate(type, subpath, source_position = nil, descrip = nil)
+    source_position ||= mock(:source_position)
+    @set.require_aggregate(type, subpath, source_position, descrip)
     source_position
   end
   
   it "should output a single fragment correctly" do
     source_position_1 = add_fragment(:javascript, "foo/bar", "/foo2/bar2", "baz", [ "bonko" ])
     
-    @output_handler_class.should_receive(:new).once.ordered.with(@asset_aggregator, { }).and_return(@output_handler)
+    call_include_text do
+      assert_reference_list_calls(@output_handler, @javascript_type, [
+        'bonko', [
+          { :descrip => "baz", :source_position => source_position_1, :file => "/foo2/bar2", :line => nil, :type => :javascript }
+        ]
+      ])
+    end
+  end
+  
+  it "should default 'descrip' to 'explicit reference', and parse out String source-positions" do
+    source_position_1 = add_fragment(:javascript, "foo/bar:173", "/foo2/bar2", nil, [ "bonko" ])
     
-    @output_handler.should_receive(:start_all).once.ordered
-    assert_reference_list_calls(@output_handler, @javascript_type, [
-      'bonko', [
-        { :descrip => "baz", :source_position => source_position_1, :file => "/foo2/bar2", :line => nil, :type => :javascript }
-      ]
-    ])
-    @output_handler.should_receive(:end_all).once.ordered
-    @output_handler.should_receive(:text).once.ordered.and_return("abcdef")
-    
-    text = @set.include_text(:output_handler_class => @output_handler_class)
-    text.should == "abcdef"
+    call_include_text do
+      assert_reference_list_calls(@output_handler, @javascript_type, [
+        'bonko', [
+          { :descrip => "explicit reference", :source_position => source_position_1, :file => "/foo2/bar2", :line => 173, :type => :javascript }
+        ]
+      ])
+    end
   end
   
   it "should output two fragments in the same subpath correctly" do
     source_position_1 = add_fragment(:javascript, "foo/bar", "/foo2/bar2", "descrip1", [ "bonko" ])
     source_position_2 = add_fragment(:javascript, "bar/baz", "/bar2/baz2", "descrip2", [ "bonko" ])
     
-    @output_handler_class.should_receive(:new).once.ordered.with(@asset_aggregator, { }).and_return(@output_handler)
+    call_include_text do
+      assert_reference_list_calls(@output_handler, @javascript_type, [
+        'bonko', [
+          { :descrip => "descrip2", :source_position => source_position_2, :file => "/bar2/baz2", :line => nil, :type => :javascript },
+          { :descrip => "descrip1", :source_position => source_position_1, :file => "/foo2/bar2", :line => nil, :type => :javascript }
+        ]
+      ])
+    end
+  end
+  
+  it "should output two fragments in the same subpath, and one in another, correctly" do
+    source_position_1 = add_fragment(:javascript, "foo/bar", "/foo2/bar2", "descrip1", [ "bonko" ])
+    source_position_2 = add_fragment(:javascript, "bar/baz", "/bar2/baz2", "descrip2", [ "bonko" ])
+    source_position_3 = add_fragment(:javascript, "a/b", "/a2/b2", "descrip3", [ "other" ])
     
+    call_include_text do
+      assert_reference_list_calls(@output_handler, @javascript_type, [
+        'bonko', [
+          { :descrip => "descrip2", :source_position => source_position_2, :file => "/bar2/baz2", :line => nil, :type => :javascript },
+          { :descrip => "descrip1", :source_position => source_position_1, :file => "/foo2/bar2", :line => nil, :type => :javascript }
+        ],
+        'other', [
+          { :descrip => "descrip3", :source_position => source_position_3, :file => "/a2/b2", :line => nil, :type => :javascript },
+        ]
+      ])
+    end
+  end
+  
+  it "should output direct aggregate references" do
+    source_position_1 = add_aggregate(:javascript, 'foobar', "sp1", 'zzz')
+    source_position_2 = add_aggregate(:javascript, 'foobar', "sp2", 'yyy')
+    
+    call_include_text do
+      assert_reference_list_calls(@output_handler, @javascript_type, [
+        'foobar', [
+          { :descrip => 'zzz', :source_position => "sp1", :file => 'foobar', :type => :javascript, :is_aggregate => true },
+          { :descrip => 'yyy', :source_position => "sp2", :file => 'foobar', :type => :javascript, :is_aggregate => true }
+        ]
+      ])
+    end
+  end
+  
+  it "should allow mixing fragment and direct aggregate references" do
+    source_position_1 = add_aggregate(:javascript, 'foobar', "sp1", 'zzz')
+    source_position_2 = add_fragment(:javascript, 'foo/bar', '/foo2/bar2', 'descrip2', [ 'foobar' ])
+    source_position_3 = add_fragment(:javascript, 'a/b', '/a3/b3', 'descrip3', [ 'barfoo' ])
+    source_position_4 = add_aggregate(:javascript, 'barbar', "sp4", 'xxx')
+    
+    call_include_text do
+      assert_reference_list_calls(@output_handler, @javascript_type, [
+        'barbar', [
+          { :descrip => 'xxx', :source_position => "sp4", :file => 'barbar', :type => :javascript, :is_aggregate => true }
+        ],
+        'barfoo', [
+          { :descrip => 'descrip3', :source_position => source_position_3, :file => '/a3/b3', :line => nil, :type => :javascript }
+        ],
+        'foobar', [
+          { :descrip => "descrip2", :source_position => source_position_2, :file => "/foo2/bar2", :line => nil, :type => :javascript },
+          { :descrip => 'zzz', :source_position => "sp1", :file => 'foobar', :type => :javascript, :is_aggregate => true }
+        ]
+      ])
+    end
+  end
+  
+  describe "with multiple types, but selecting just one" do
+    before :each do
+      @source_position_1 = add_fragment(:javascript, 'foo/bar', '/foo1/bar1', 'descrip1', [ 'path1' ], nil, true)
+      @source_position_2 = add_fragment(:css, 'bar/baz', '/bar2/baz2', 'descrip2', [ 'path1' ], nil, true)
+    
+      @asset_aggregator.should_receive(:aggregated_subpaths_for).any_number_of_times.with(any_args).and_return([ 'path1' ])
+    end
+    
+    it "should allow selecting just CSS" do
+      call_include_text(:types => [ :css ]) do
+        assert_reference_list_calls(@output_handler, @css_type, [
+          'path1', [
+            { :descrip => "descrip2", :source_position => @source_position_2, :file => "/bar2/baz2", :line => nil, :type => :css }
+          ]
+        ])
+      end
+    end
+
+    it "should allow selecting just Javascript" do
+      call_include_text(:types => [ :javascript ]) do
+        assert_reference_list_calls(@output_handler, @javascript_type, [
+          'path1', [
+            { :descrip => "descrip1", :source_position => @source_position_1, :file => "/foo1/bar1", :line => nil, :type => :javascript }
+          ]
+        ])
+      end
+    end
+  end
+  
+  it "should output nothing, but still give callbacks, for explicitly-selected types with no content" do
+    source_position_1 = add_fragment(:javascript, 'foo/bar', '/foo1/bar1', 'descrip1', [ 'path1' ], nil, true)
+    
+    call_include_text(:types => [ :css ]) do
+      assert_reference_list_calls(@output_handler, @css_type, [ ])
+    end
+  end
+  
+  it "should pass arbitrary options through to the PageReferencesOutputHandler constructor" do
+    source_position_1 = add_fragment(:javascript, "foo/bar", "/foo2/bar2", "baz", [ "bonko" ])
+    
+    call_include_text(:foo => :bar, :baz => [ :quux ]) do
+      assert_reference_list_calls(@output_handler, @javascript_type, [
+        'bonko', [
+          { :descrip => "baz", :source_position => source_position_1, :file => "/foo2/bar2", :line => nil, :type => :javascript }
+        ]
+      ])
+    end
+  end
+  
+  def call_include_text(options = { })
+    @output_handler_class.should_receive(:new).once.ordered.with(@asset_aggregator, options.reject { |k,v| [ :types ].include?(k) }).and_return(@output_handler)
     @output_handler.should_receive(:start_all).once.ordered
-    assert_reference_list_calls(@output_handler, @javascript_type, [
-      'bonko', [
-        { :descrip => "descrip2", :source_position => source_position_2, :file => "/bar2/baz2", :line => nil, :type => :javascript },
-        { :descrip => "descrip1", :source_position => source_position_1, :file => "/foo2/bar2", :line => nil, :type => :javascript }
-      ]
-    ])
-    @output_handler.should_receive(:end_all).once.ordered
-    @output_handler.should_receive(:text).once.ordered.and_return("abcdef")
     
-    text = @set.include_text(:output_handler_class => @output_handler_class)
-    text.should == "abcdef"
+    yield
+    
+    @output_handler.should_receive(:end_all).once.ordered
+    output_text = "abcdef_#{rand(1_000_000)}"
+    @output_handler.should_receive(:text).once.ordered.and_return(output_text)
+    
+    text = @set.include_text({ :output_handler_class => @output_handler_class }.merge(options))
+    text.should == output_text
   end
   
   def assert_reference_list_calls(object, expected_type, subpath_and_expected_references_array)
@@ -107,15 +245,23 @@ describe AssetAggregator::Core::PageReferenceSet do
     
     actual_references.each_with_index do |actual_reference, index|
       expected_reference = expected_references[index]
-      check_reference(actual_reference, expected_reference[:descrip], expected_reference[:source_position], expected_reference[:file], expected_reference[:line], expected_reference[:type])
+      check_reference(actual_reference, expected_reference[:descrip], expected_reference[:source_position], expected_reference[:file], expected_reference[:line], expected_reference[:type], expected_reference[:is_aggregate] ? :aggregate : :fragment)
     end
   end
   
-  def check_reference(reference, descrip, source_position, file, line, type)
+  def check_reference(reference, descrip, source_position, file, line, type, reference_type)
     reference.descrip.should == descrip
     reference.reference_source_position.should == source_position
-    reference.fragment_source_position.file.should == file
-    reference.fragment_source_position.line.should == line
+    
+    if reference_type == :fragment
+      reference.kind_of?(AssetAggregator::Core::FragmentReference).should be_true
+      reference.fragment_source_position.file.should == file
+      reference.fragment_source_position.line.should == line
+    else
+      reference.kind_of?(AssetAggregator::Core::AggregateReference).should be_true
+      reference.subpath.should == file
+    end
+    
     reference.aggregate_type.should == type
   end
 end
